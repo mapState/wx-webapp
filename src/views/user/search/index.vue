4c36c39b6d0110a828c69d9ca5ffc720254e9dd0
wini <1290143341@qq.com>
2019-12-12 10:11:09 +0800

    搜索

--- src/views/user/search/index.vue
+++ src/views/user/search/index.vue
@@ -1,7 +1,9 @@
 <template>
   <div v-wechat-title="$route.meta.title" class="container">
     <div class="searchTop">
-      商家名称
+      <van-search placeholder="商品名称" v-model="msg" @focus="historyShow=true" @blur="historyShow=false" @search="onSearch"/>
+      
+      <img src="@/assets/left.png" class="upDown" @click="$router.go(-1)" />
     </div>
 
     <div class="list">
@@ -28,21 +30,57 @@
         <div class="num">600m</div>
       </div>
     </div>
+    <van-popup v-model="historyShow" position="top">
+      <div class="history">
+        <div class="title">
+          搜索记录
+          <div class="right" @click="clear">
+            <img src="@/assets/del.png" class="upDown"/>
+            清空
+          </div>
+        </div>
+        <div class="content">
+          <div class="it" v-for="(item,i) in historyList" :key="i" @click="textIn(item)">{{item}}</div>
+        </div>
+      </div>
+    </van-popup>
   </div>
 </template>
 
 <script>
+import { SetCookie,GetCookie } from "@/utils/utils";
 export default {
   data() {
     return {
-      sort:1
+      sort: 1,
+      msg: "",
+      historyShow: false,
+      historyList:[]
     };
   },
   methods: {
-    chooseSort(e){
-      this.sort=e
+    chooseSort(e) {
+      this.sort = e;
     },
-    init() {}
+    onSearch(){
+      this.historyList.push(this.msg)
+      SetCookie('history',this.historyList.toString())
+    },
+    textIn(t){
+      this.msg=t;
+      this.onSearch()
+    },
+    clear(){
+      this.historyList=[]
+      SetCookie('history',this.historyList.toString())
+    },
+    init() {
+      try {
+        this.historyList=GetCookie('history').split(',')
+      } catch (error) {
+        
+      }
+    }
   },
 
   mounted() {
@@ -62,7 +100,7 @@ export default {
   padding: 0;
   border: 1px solid rgba(221, 221, 221, 1);
   border-radius: 3px;
-  width: 346px;
+  width: 307px;
   height: 36px;
   margin: 0 auto;
 }
